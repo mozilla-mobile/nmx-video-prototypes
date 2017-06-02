d73c0806a6efafde6cb76f5b7578e0b19fa736b7
Michael Comella <michael.l.comella@gmail.com>
2017-06-01 17:38:20 -0700

    Add playlist recyclerView with dummy data.

--- app/src/main/java/org/mozilla/prototypevideoplaylists/PlaylistActivity.kt
+++ app/src/main/java/org/mozilla/prototypevideoplaylists/PlaylistActivity.kt
@@ -4,13 +4,54 @@
 
 package org.mozilla.prototypevideoplaylists
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_playlist.*
 
 class PlaylistActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_playlist)
+        initToolbar()
+        initPlaylistView()
     }
+
+    private fun initToolbar() {
+        setSupportActionBar(toolbar)
+        val appBar = supportActionBar!!
+        appBar.title = resources.getString(R.string.playlist_activity_title)
+    }
+
+    private fun initPlaylistView() {
+        playlistView.adapter = PlaylistAdapter(this)
+        playlistView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+    }
+}
+
+private class PlaylistAdapter(val context: Context) : RecyclerView.Adapter<PlaylistViewHolder>() {
+    override fun onBindViewHolder(holder: PlaylistViewHolder?, position: Int) {
+        if (holder == null) return
+        holder.titleView.text = "Test title"
+    }
+
+    override fun getItemCount(): Int {
+        return 1
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): PlaylistViewHolder {
+        val view = LayoutInflater.from(parent!!.context).inflate(R.layout.playlist_item, parent, false)
+        return PlaylistViewHolder(view)
+    }
+}
+
+private class PlaylistViewHolder(val rootView: View) : RecyclerView.ViewHolder(rootView) {
+    val titleView = rootView.findViewById(R.id.playlistTitleView) as TextView
 }
